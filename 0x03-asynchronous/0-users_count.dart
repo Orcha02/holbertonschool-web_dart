c7f05231e49c1a960a1a41fce70a4671e3e5cd59
Orcha02 <orcha2002@gmail.com>
2022-08-08 13:53:27 -0500

    Delete all project

--- 0x03-asynchronous/0-users_count.dart
+++
@@ -1,8 +0,0 @@
-Future<int> fetchUsersCount() => Future.delayed(
-      const Duration(seconds: 2),
-      () => 19,
-    );
-
-usersCount() async {
-  print(await fetchUsersCount());
-}
--- 0x03-asynchronous/README.md
+++
@@ -1,1 +0,0 @@
-# DartDart
